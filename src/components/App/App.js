339ce7bc8e32948b803ac6dea758d5065bbefb7a
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-11 16:45:19 -0500

    create route to main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Header from "../Header/Header";
+import Main from '../Main';
 
 function App() {
   return (
     <div className="App">
-      app
+    <Header/>
+    <Main/>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import PostPage from "../pages/post/PostPage"
+import Show from "../pages/show/Show"
+
+
 const Main = () => {
     return (
-<div>Main</div>
+<main>
+<Routes>
+      <Route path="/" element={<PostPage/>} />
+      <Route path="/post/:id" element={<Show/>} />
+
+</Routes>
+</main>
     )
 }
 
