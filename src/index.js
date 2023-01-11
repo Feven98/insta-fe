1fcdef2162ec3092dbb4f383be34e63725f56c45
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-11 16:16:24 -0500

    installing dependency

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App/App'
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
 
 
